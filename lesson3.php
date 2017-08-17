8dd3ac5959df96d897f3a08afa283556c5692853
HannaVaitovich <30897096+HannaVaitovich@users.noreply.github.com>
2017-08-17 16:59:27 -0500

    Update lesson3.php

--- lesson3.php
+++ lesson3.php
@@ -19,10 +19,13 @@ $animals = [
 
 $animals_array = [];
 $array_2word = [];
-foreach ($animals as $continent => $names) {
-	foreach ($names as $animal) {
+foreach ($animals as $continent => $names) 
+{
+	foreach ($names as $animal) 
+	{
 		$animalnames = explode (" ", $animal);
-		if (count($animalnames) == 2) {
+		if (count($animalnames) == 2) 
+		{
 	$animals_array[$continent][] = join (" ", $animalnames);
 	$array_2word[] = $animalnames[1];
 		}
